de6fe7e61c0de2769df4edad996ae285534d9a88
sere06001 <sere06001@utb.vaxjo.se>
2025-03-11 10:54:22 +0100

    settings method, change naming stuff

--- Managers/MenuScreen.cs
+++ Managers/MenuScreen.cs
@@ -1,8 +1,8 @@
 namespace GA_2d_shooter;
 public class MenuScreen
 {
-    private readonly Game1 _game;
-    private readonly string[] _menuItems = { "Start Game", "Options", "Exit" };
+    private readonly Game1 game;
+    private readonly string[] menuItems = { "Start Game", "Options", "Exit" };
     private int _selectedIndex;
     private readonly Vector2[] _menuPositions;
     private readonly Color _normalColor = Color.White;
@@ -10,12 +10,12 @@ public class MenuScreen
     private KeyboardState _prevKeyState;
     public MenuScreen(Game1 game)
     {
-        _game = game;
-        _menuPositions = new Vector2[_menuItems.Length];
+        game = this.game;
+        _menuPositions = new Vector2[menuItems.Length];
         // Calculate center positions for menu items
         float centerX = Globals.Bounds.X / 2;
         float startY = Globals.Bounds.Y / 3;
-        for (int i = 0; i < _menuItems.Length; i++)
+        for (int i = 0; i < menuItems.Length; i++)
         {
             _menuPositions[i] = new Vector2(
                 centerX,
@@ -27,21 +27,21 @@ public class MenuScreen
     {
         KeyboardState currentKeyState = Keyboard.GetState();
         if (currentKeyState.IsKeyDown(Keys.Down) && !_prevKeyState.IsKeyDown(Keys.Down))
-            _selectedIndex = (_selectedIndex + 1) % _menuItems.Length;
+            _selectedIndex = (_selectedIndex + 1) % menuItems.Length;
         if (currentKeyState.IsKeyDown(Keys.Up) && !_prevKeyState.IsKeyDown(Keys.Up))
-            _selectedIndex = (_selectedIndex - 1 + _menuItems.Length) % _menuItems.Length;
+            _selectedIndex = (_selectedIndex - 1 + menuItems.Length) % menuItems.Length;
         if (currentKeyState.IsKeyDown(Keys.Enter) && !_prevKeyState.IsKeyDown(Keys.Enter))
         {
             switch (_selectedIndex)
             {
-                case 0: // Start Game
-                    ((Game1)_game).isInMenu = false;
+                case 0: //Start Game
+                    ((Game1)game).isInMenu = false;
                     break;
-                case 1: // Options
-                    // Add options menu logic here
+                case 1: //Settings
+                    ;
                     break;
-                case 2: // Exit
-                    _game.Exit();
+                case 2: //Exit
+                    game.Exit();
                     break;
             }
         }
@@ -50,13 +50,13 @@ public class MenuScreen
     public void Draw(SpriteBatch spriteBatch)
     {
         Vector2 textMiddlePoint;
-        for (int i = 0; i < _menuItems.Length; i++)
+        for (int i = 0; i < menuItems.Length; i++)
         {
             Color color = (i == _selectedIndex) ? _selectedColor : _normalColor;
-            textMiddlePoint = Globals.Font.MeasureString(_menuItems[i]) / 2;
+            textMiddlePoint = Globals.Font.MeasureString(menuItems[i]) / 2;
             spriteBatch.DrawString(
                 Globals.Font,
-                _menuItems[i],
+                menuItems[i],
                 _menuPositions[i],
                 color,
                 0f,
